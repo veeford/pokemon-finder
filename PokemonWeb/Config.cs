db5e371e9ae4a648adbc5e48f197b9d99bfda825
Абрамов Сергей Дмитриевич <vincefordmh@mail.ru>
2023-11-13 22:30:28 +0500

    Add FTP support - add API path for sending pokemon's data to FTP server - add method `SaveDataToFtp(Pokemon pokemon)`

--- PokemonWeb/Config.cs
+++ PokemonWeb/Config.cs
@@ -5,5 +5,9 @@ namespace PokemonWeb
     {
         public static string EmailAddress;
         public static string EmailPasswd;
+
+        public static string FtpAddress;
+        public static string FtpUsername;
+        public static string FtpPassword;
     }
 }
